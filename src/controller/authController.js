1bc22d8a16df93c05fd7f116b48fc5f6ceb40993
siavashmosadegh <s.mosadegh@rocketmail.com>
2026-02-15 20:10:31 +0330

    improved authController

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -2,8 +2,13 @@ import { registerService } from "../services/authServices.js";
 
 export const register = async (req , res , next ) => {
     try {
-        const result = await registerService(req.body);
-        res.status(201).json("registered Successfully");
+        const {user, token} = await registerService(req.body);
+        res.status(201).json({
+            success: true,
+            message: "Registered Successfully",
+            user,
+            token
+        });
     } catch (error) {
         next(error);
     }

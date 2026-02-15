d858f8b94da640a08a62328d2ede41634ab3fac8
Siavash <s.mosadegh@rocketmail.com>
2026-02-15 16:24:41 +0330

    authService added ...

---
+++ src/services/authServices.js
@@ -0,0 +1,37 @@
+import { findByEmail } from "../repository/authRepository";
+import { hash, sign } from "bcrypt";
+
+export const registerService = async (data) => {
+
+    console.log(data);
+
+    // 1. validation
+    // check to see if user exists
+    
+    const existingUser = await findByEmail(data.email);
+
+    if (existingUser) {
+        throw new Error ("this user already exists");
+    }
+
+    // 2. hash password
+
+    const hashedPassword = await hash(password,10);
+
+    // 3. save to db
+
+    const result = await insertUser(data)
+    
+    // 4. generate token
+
+    const token = sign({ id: data.email }, process.env.JWT_SECRET, {
+        expiresIn: '1h'
+    })
+
+    // 5. send response
+
+    return token;
+
+    //const user = await authRepository.createUser(data);
+    //return user;
+};
